a61bbc37b07083b1bb52b4ece1b408ad4cb40963
Rever <rever22411@gmail.com>
2020-07-08 11:23:13 +0200

    Updates

---
+++ precache-manifest.9d8f1352875b2d17063380c032f2cd48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab705598957437d5ff3e65783be0e178",
+    "url": "/nuvola-punti/index.html"
+  },
+  {
+    "revision": "d87d8a94eef87afda260",
+    "url": "/nuvola-punti/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "904526580690be506f74",
+    "url": "/nuvola-punti/static/css/main.f25c5fea.chunk.css"
+  },
+  {
+    "revision": "d87d8a94eef87afda260",
+    "url": "/nuvola-punti/static/js/2.51a84988.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/nuvola-punti/static/js/2.51a84988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "904526580690be506f74",
+    "url": "/nuvola-punti/static/js/main.f06692e4.chunk.js"
+  },
+  {
+    "revision": "24939a35ca92ac4c13a9",
+    "url": "/nuvola-punti/static/js/runtime-main.2eba8eff.js"
+  }
+]);
